33c55cb111166a47560878ca097ec00c95bcbf5e
Derek Martins - Computação Didática <82281030+computacaodidatica@users.noreply.github.com>
2021-09-11 22:03:34 -0300

    Solução Suficiente para passar no teste

--- solution.py
+++ solution.py
@@ -2,20 +2,27 @@ class Solution:
 	# @param A : string
 	# @return an integer
     def numDecodings(self, A):
-
-        if A[0] == 0:
+        
+        if(A[0] == "0"):
             return 0
-
-        t = [1]
-
+        
+        t = [1, 1]
+    
         for x in range(1, len(A)):
-
-            if (int(A[x-1]) in [1,2]) and (int(A[x-1] + A[x]) % 10 == 0):
-                return 0
+            
+            if A[x] == "0":
+                if A[x-1] in "12":
+                   t.pop()
+                  
+                else:
+                    return 0
+           
+            
+            elif int(A[x-1] + A[x]) in range(1, 27) and A[x-1] != "0" or A [x] == 0:
+                t.append((t[-1] + t[-2]) % (10 ** 9 + 7))
                 
-            if int(A[x-1] + A[x]) in range(0, 27):
-                t.append((t[-1] + 2) % (10 ** 9 + 7))
+                
             else:
-                t.append(t[-1]) 
+                t.append(t[-1])
                 
         return t[-1]
